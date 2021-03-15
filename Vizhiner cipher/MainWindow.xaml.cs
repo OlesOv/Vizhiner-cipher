3be87ab8082752710ace6e739b47cdbc98e1a78a
OlesOv <66430622+OlesOv@users.noreply.github.com>
2021-03-15 19:08:58 +0200

    Made code more understandable, but now it`s not working

--- Vizhiner cipher/MainWindow.xaml.cs
+++ Vizhiner cipher/MainWindow.xaml.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Vizhiner_cipher
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    static class Chars
+    {
+        public static string RussianAlphabet = "абвгдеёжзийклмнопрстуфхцшщъыьэюя",
+            EnglishAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    }
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -26,179 +21,80 @@ namespace Vizhiner_cipher
             InitializeComponent();
         }
 
-        private string[] Alphabets = { "абвгдеёжзийклмнопрстуфхцшщъыьэюя", "abcdefghijklmnopqrstuvwxyz",
-            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ", "ABCDEFGHIJKLMOPQRSTUVWXYZ", " ?!., \n\r" };
-
-        private string Encrypt(string text)
+        private string Encrypt(string text, string keyword, string alphabet)
         {
-            string result = "", key = "";
-            int j = 0;
+            string result = "";
+            int lastKeyChar = 0;
             for (int i = 0; i < text.Length; i++)
             {
-                if(Alphabets[4].IndexOf(text[i]) < 0)
+                if (alphabet.Contains(text[i]))
                 {
-                    key += KeywordTextBox.Text.ToLower()[j % KeywordTextBox.Text.Length];
-                    j++;
+                    result += alphabet[(alphabet.IndexOf(text[i]) + 
+                        alphabet.IndexOf(keyword[lastKeyChar % keyword.Length])) % alphabet.Length];
+                    lastKeyChar++;
                 }
                 else
                 {
-                    key += '_';
-                }
-            }
-            //Russian
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (LanguageComboBox.SelectedIndex == 0)
-                {
-                    if (Alphabets[0].IndexOf(text[i]) >= 0)  //Lower Case
-                    {
-                        result += Alphabets[0][(Alphabets[0].IndexOf(text[i]) +
-                            Alphabets[0].IndexOf(key[i])) % Alphabets[0].Length];
-                    }
-                    else if (Alphabets[2].IndexOf(text[i]) >= 0) //Upper Case
-                    {
-                        result += Alphabets[2][(Alphabets[2].IndexOf(text[i]) +
-                            Alphabets[0].IndexOf(key[i])) % Alphabets[2].Length];
-                    }
-                    else
-                    {
-                        result += text[i];
-                    }
-                }
-                //English
-                else
-                {
-                    if (Alphabets[1].IndexOf(text[i]) >= 0)  //Lower Case
-                    {
-                        result += Alphabets[1][(Alphabets[1].IndexOf(text[i]) +
-                            Alphabets[1].IndexOf(key[i])) % Alphabets[1].Length];
-                    }
-                    else if (Alphabets[3].IndexOf(text[i]) >= 0) //Upper Case
-                    {
-                        result += Alphabets[3][(Alphabets[3].IndexOf(text[i]) +
-                            Alphabets[1].IndexOf(key[i])) % Alphabets[3].Length];
-                    }
-                    else
-                    {
-                        result += text[i];
-                    }
+                    result += text[i];
                 }
             }
             return result;
         }
 
-        private string Decrypt(string text)
+        private static string Decrypt(string text, string keyword, string alphabet)
         {
-            string result = "", key = "";
-            int j = 0;
+            string result = "";
+            int lastKeyChar = 0;
             for (int i = 0; i < text.Length; i++)
             {
-                if (Alphabets[4].IndexOf(text[i]) < 0)
+                if (alphabet.Contains(text[i]))
                 {
-                    key += KeywordTextBox.Text.ToLower()[j % KeywordTextBox.Text.Length];
-                    j++;
+                    result += alphabet[(alphabet.IndexOf(text[i]) + alphabet.Length -
+                        alphabet.IndexOf(keyword[lastKeyChar % keyword.Length])) % alphabet.Length];
+                    lastKeyChar++;
                 }
                 else
                 {
-                    key += '_';
-                }
-            }
-            //Russian
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (LanguageComboBox.SelectedIndex == 0)
-                {
-                    if (Alphabets[0].IndexOf(text[i]) >= 0)  //Lower Case
-                    {
-                        result += Alphabets[0][(Alphabets[0].IndexOf(text[i]) + Alphabets[0].Length -
-                            Alphabets[0].IndexOf(key[i])) % Alphabets[0].Length];
-                    }
-                    else if (Alphabets[2].IndexOf(text[i]) >= 0) //Upper Case
-                    {
-                        result += Alphabets[2][(Alphabets[2].IndexOf(text[i]) + Alphabets[2].Length -
-                            Alphabets[0].IndexOf(key[i])) % Alphabets[2].Length];
-                    }
-                    else
-                    {
-                        result += text[i];
-                    }
-                }
-                //English
-                else
-                {
-                    if (Alphabets[1].IndexOf(text[i]) >= 0)  //Lower Case
-                    {
-                        result += Alphabets[1][(Alphabets[1].IndexOf(text[i]) + Alphabets[1].Length -
-                            Alphabets[1].IndexOf(key[i])) % Alphabets[1].Length];
-                    }
-                    else if (Alphabets[3].IndexOf(text[i]) >= 0) //Upper Case
-                    {
-                        result += Alphabets[3][(Alphabets[3].IndexOf(text[i]) + Alphabets[3].Length -
-                            Alphabets[1].IndexOf(key[i])) % Alphabets[3].Length];
-                    }
-                    else
-                    {
-                        result += text[i];
-                    }
+                    result += text[i];
                 }
             }
             return result;
-        }
-
-        private Regex[] Regexes = { new Regex(@"^[?!,.а-яА-ЯёЁ0-9\s]+$"), new Regex(@"^[?!,.a-zA-Z0-9\s]+$") };
-
-        private void OriginalTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !Regexes[LanguageComboBox.SelectedIndex].IsMatch(e.Text);
-            if (!e.Handled)
-            {
-                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text + e.Text);
-            }
-        }
-
-        private void EncryptedTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !Regexes[LanguageComboBox.SelectedIndex].IsMatch(e.Text);
-            if (!e.Handled)
-            {
-                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text + e.Text);
-            }
         }
 
         private void KeywordTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            e.Handled = !Regexes[LanguageComboBox.SelectedIndex].IsMatch(e.Text);
+            e.Handled = !Chars.EnglishAlphabet.Contains(e.Text);
             if (!e.Handled)
             {
                 EncryptedTextBox.IsReadOnly = false;
                 OriginalTextBox.IsReadOnly = false;
             }
-            EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text);
-            DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text);
+            EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Chars.EnglishAlphabet);
+            DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Chars.EnglishAlphabet);
         }
 
         private void LanguageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(EncryptedTextBox != null)
+            if (EncryptedTextBox != null)
             {
                 EncryptedTextBox.Text = "";
                 EncryptedTextBox.IsReadOnly = true;
             }
-            if (KeywordTextBox != null) KeywordTextBox.Text = "";
-            if (DecryptedTextBox != null) DecryptedTextBox.Text = "";
-            if (OriginalTextBox != null) OriginalTextBox.IsReadOnly = true;
-        }
+            if (KeywordTextBox != null)
+            {
+                KeywordTextBox.Text = "";
+            }
 
-        //private void KeywordTextBox_KeyDown(object sender, KeyEventArgs e)
-        //{
-        //    if(e.Key == Key.Back && KeywordTextBox.Text.Length == 1)
-        //    {
-        //        EncryptedTextBox.Text = "";
-        //        OriginalTextBox.Text = "";
-        //        DecryptedTextBox.Text = "";
-        //        EncryptedTextBox.IsReadOnly = true;
-        //        OriginalTextBox.IsReadOnly = true;
-        //    }
-        //}
+            if (DecryptedTextBox != null)
+            {
+                DecryptedTextBox.Text = "";
+            }
+
+            if (OriginalTextBox != null)
+            {
+                OriginalTextBox.Text = "";
+                OriginalTextBox.IsReadOnly = true;
+            }
+        }
     }
 }
