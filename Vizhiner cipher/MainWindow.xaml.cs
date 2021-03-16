4a0c2649110d90562932d65575956b0d9b6d1589
OlesOv <66430622+OlesOv@users.noreply.github.com>
2021-03-16 11:08:01 +0200

    Everything seems to work

--- Vizhiner cipher/MainWindow.xaml.cs
+++ Vizhiner cipher/MainWindow.xaml.cs
@@ -1,15 +1,16 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Vizhiner_cipher
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    static class Chars
+    static class Alphabets
     {
-        public static string RussianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+        public const string RussianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя",
             EnglishAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
     }
@@ -25,14 +26,24 @@ namespace Vizhiner_cipher
 
         private static string Encrypt(string text, string keyword, string alphabet)
         {
-            string result = "";
+            string result = "", originalText = text;
             int lastKeyChar = 0;
+            text = text.ToLower();
             for (int i = 0; i < text.Length; i++)
             {
                 if (alphabet.Contains(text[i]))
                 {
-                    result += alphabet[(alphabet.IndexOf(text[i]) + 
+                    char temp = alphabet[(alphabet.IndexOf(text[i]) +
                         alphabet.IndexOf(keyword[lastKeyChar % keyword.Length])) % alphabet.Length];
+
+                    if(originalText[i] == char.ToUpper(text[i]))
+                    {
+                        result += char.ToUpper(temp);
+                    }
+                    else
+                    {
+                        result += temp;
+                    }
                     lastKeyChar++;
                 }
                 else
@@ -45,14 +56,24 @@ namespace Vizhiner_cipher
 
         private static string Decrypt(string text, string keyword, string alphabet)
         {
-            string result = "";
+            string result = "", originalText = text;
             int lastKeyChar = 0;
+            text = text.ToLower();
             for (int i = 0; i < text.Length; i++)
             {
                 if (alphabet.Contains(text[i]))
                 {
-                    result += alphabet[(alphabet.IndexOf(text[i]) + alphabet.Length -
+                    char temp = alphabet[(alphabet.IndexOf(text[i]) + alphabet.Length -
                         alphabet.IndexOf(keyword[lastKeyChar % keyword.Length])) % alphabet.Length];
+
+                    if (originalText[i] == char.ToUpper(text[i]))
+                    {
+                        result += char.ToUpper(temp);
+                    }
+                    else
+                    {
+                        result += temp;
+                    }
                     lastKeyChar++;
                 }
                 else
@@ -67,21 +88,23 @@ namespace Vizhiner_cipher
         {
             if (LanguageComboBox.SelectedIndex == RussianLanguageIndex)
             {
-                e.Handled = !Chars.RussianAlphabet.Contains(e.Text);
+                e.Handled = !Alphabets.RussianAlphabet.Contains(e.Text);
             }
             else if (LanguageComboBox.SelectedIndex == EnglishLanguageIndex)
             {
-                e.Handled = !Chars.EnglishAlphabet.Contains(e.Text);
+                e.Handled = !Alphabets.EnglishAlphabet.Contains(e.Text);
             }
 
             if (!e.Handled)
             {
                 EncryptedTextBox.IsReadOnly = false;
                 OriginalTextBox.IsReadOnly = false;
+                EncryptedTextBox.Background = Brushes.White;
+                OriginalTextBox.Background = Brushes.White;
             }
             else
             {
-                MessageBox.Show("Ensure that you selected a correct language and you are writing in a lower-case");
+                MessageBox.Show("Ensure that you selected a correct language and that you are writing in a lowercase");
             }
         }
 
@@ -91,6 +114,8 @@ namespace Vizhiner_cipher
             {
                 EncryptedTextBox.IsReadOnly = true;
                 OriginalTextBox.IsReadOnly = true;
+                EncryptedTextBox.Background = Brushes.LightGray;
+                OriginalTextBox.Background = Brushes.LightGray;
                 EncryptedTextBox.Text = "";
                 OriginalTextBox.Text = "";
                 DecryptedTextBox.Text = "";
@@ -98,13 +123,13 @@ namespace Vizhiner_cipher
 
             if (LanguageComboBox.SelectedIndex == RussianLanguageIndex)
             {
-                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Chars.RussianAlphabet);
-                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Chars.RussianAlphabet);
+                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Alphabets.RussianAlphabet);
+                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Alphabets.RussianAlphabet);
             }
             else if (LanguageComboBox.SelectedIndex == EnglishLanguageIndex)
             {
-                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Chars.EnglishAlphabet);
-                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Chars.EnglishAlphabet);
+                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Alphabets.EnglishAlphabet);
+                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Alphabets.EnglishAlphabet);
             }
         }
 
@@ -136,11 +161,11 @@ namespace Vizhiner_cipher
         {
             if(LanguageComboBox.SelectedIndex == RussianLanguageIndex)
             {
-                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Chars.RussianAlphabet);
+                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Alphabets.RussianAlphabet);
             }
             else if(LanguageComboBox.SelectedIndex == EnglishLanguageIndex)
             {
-                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Chars.EnglishAlphabet);
+                EncryptedTextBox.Text = Encrypt(OriginalTextBox.Text, KeywordTextBox.Text, Alphabets.EnglishAlphabet);
             }
         }
 
@@ -148,11 +173,11 @@ namespace Vizhiner_cipher
         {
             if (LanguageComboBox.SelectedIndex == RussianLanguageIndex)
             {
-                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Chars.RussianAlphabet);
+                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Alphabets.RussianAlphabet);
             }
             else if (LanguageComboBox.SelectedIndex == EnglishLanguageIndex)
             {
-                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Chars.EnglishAlphabet);
+                DecryptedTextBox.Text = Decrypt(EncryptedTextBox.Text, KeywordTextBox.Text, Alphabets.EnglishAlphabet);
             }
         }
     }
